91756499b481611fe4555226d88ef3c78c11bfdc
Ubuntu <ubuntu@ip-10-248-105-37.us-west-2.compute.internal>
2013-07-09 03:27:48 +0000

    Edited web.js to display contents of index.html again

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var idxContents = fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(idxContents.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
