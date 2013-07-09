bfa1fa40b836717626021044f765fc364d7fd9e9
Samuel Harrold <samuel.harrold@gmail.com>
2013-07-09 03:52:53 +0000

    Edited web.js to display contents of index.html again

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-// var idxContents = fs.readFileSync('./index.html', 'utf8');
+var idxContents = fs.readFileSync('./index.html', 'utf8');
 
 app.get('/', function(request, response) {
-//  response.send(idxContents);
-    response.send('Test.');
+    response.send(idxContents);
+//    response.send('Test.');
 
 });
 
